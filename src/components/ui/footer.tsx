ebef0b975e7490b67983215ce8267780aa56ae9a
Pedro Vitor Mantovani <83127263+Pedrinvits@users.noreply.github.com>
2023-10-28 13:56:45 -0300

    feat - fix footer

--- src/components/ui/footer.tsx
+++ src/components/ui/footer.tsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
-    <div className="bg-accent px-8 py-4 text-[0.625rem] opacity-75">
-      © 2023 Copyright <span className="font-semibold">FSW Store</span>
+    <div className="bg-accent px-8 py-4 text-[0.625rem] opacity-75 flex justify-center">
+      © 2023 Copyright <span className="font-semibold">FSW Shop</span>
     </div>
   );
 };
