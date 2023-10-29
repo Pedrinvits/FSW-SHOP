b6a4990fd850f20d5e1cb00f457da9a721cc5826
Pedro Vitor Mantovani <83127263+Pedrinvits@users.noreply.github.com>
2023-10-29 12:01:04 -0300

    fix - cart items in local storage

--- src/providers/cart.tsx
+++ src/providers/cart.tsx
@@ -35,13 +35,11 @@ export const CartContext = createContext<ICartContext>({
 });
 
 const CartProvider = ({ children }: { children: ReactNode }) => {
-  const [products, setProducts] = useState<CartProduct[]>([]);
+  const [products, setProducts] = useState<CartProduct[]>
+    (
+      JSON.parse(localStorage.getItem("@fsw-store/cart-products") ||"[]")
+    )
 
-  useEffect(() => {
-    setProducts(
-      JSON.parse(localStorage.getItem("@fsw-store/cart-products") || "[]"),
-    );
-  }, []);
 
   useEffect(() => {
     localStorage.setItem("@fsw-store/cart-products", JSON.stringify(products));
