8aff933d34849075c813bd5576dda25be4d5a66a
Pedro Vitor Mantovani <83127263+Pedrinvits@users.noreply.github.com>
2023-10-29 12:23:26 -0300

    fix - orders

--- src/app/orders/page.tsx
+++ src/app/orders/page.tsx
@@ -9,7 +9,7 @@ export const dynamic = "force-dynamic";
 
 async function OrderPage ()  {
     const session = await getServerSession(authOptions);
-    console.log(session?.user.id)
+
     if (!session || !session.user) {
         return (
           <div className="flex h-full flex-col items-center justify-center gap-2 p-5">
