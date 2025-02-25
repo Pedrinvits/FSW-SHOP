65fe356b3e5644f3177601cd357d3025b0d7e326
Pedro Mantovani <pedromantovani26@gmail.com>
2025-02-25 07:03:08 -0300

    fix : next config js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["fsw-store.s3.sa-east-1.amazonaws.com"],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '*',
+      },
+    ],
   },
   experimental: {
     serverActions: true,
