b4258f6df4fe7e689c2ac51fcb317a4b496ea77b
Pedro Mantovani <pedromantovani26@gmail.com>
2025-02-25 07:07:09 -0300

    feat : add new footer

--- src/components/ui/footer.tsx
+++ src/components/ui/footer.tsx
@@ -1,11 +1,3 @@
-// const Footer = () => {
-//   return (
-//     <div className="bg-accent px-8 py-4 text-[0.625rem] opacity-75 flex justify-center">
-//       © 2023 Copyright <span className="font-semibold">FSW Shop</span>
-//     </div>
-//   );
-// };
-
 const Footer = () => {
   return ( 
       <footer>
